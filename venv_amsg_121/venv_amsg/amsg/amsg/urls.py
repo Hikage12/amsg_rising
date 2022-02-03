95c3ec8ff9897bf1d7d622b776339e7a2661b64e
Hikage12 <grijing@gmail.com>
2022-02-03 11:37:50 +0900

    Update urls.py

--- venv_amsg_121/venv_amsg/amsg/amsg/urls.py
+++ venv_amsg_121/venv_amsg/amsg/amsg/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('allauth.urls')),
+    path('task/', include('task.urls')),
 ]
